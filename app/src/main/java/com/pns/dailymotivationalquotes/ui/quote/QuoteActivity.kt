70622a981ab433f5ba4c8b6479290314b9320c5e
Vipin dev <officialvipindev@gmail.com>
2023-01-03 12:47:54 +0530

    Update main screen

--- app/src/main/java/com/pns/dailymotivationalquotes/ui/quote/QuoteActivity.kt
+++ app/src/main/java/com/pns/dailymotivationalquotes/ui/quote/QuoteActivity.kt
@@ -1,11 +1,12 @@
-package com.pns.dailymotivationalquotes
+package com.pns.dailymotivationalquotes.ui.quote
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pns.dailymotivationalquotes.R
 
-class MainActivity : AppCompatActivity() {
+class QuoteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_quote)
     }
 }
