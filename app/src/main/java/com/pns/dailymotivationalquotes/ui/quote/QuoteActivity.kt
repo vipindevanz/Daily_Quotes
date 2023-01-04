9bd7776e2a311a41e6813b287ab3615b60fbdb9f
Vipin dev <officialvipindev@gmail.com>
2023-01-04 15:04:17 +0530

    Update quote activity

--- app/src/main/java/com/pns/dailymotivationalquotes/ui/quote/QuoteActivity.kt
+++ app/src/main/java/com/pns/dailymotivationalquotes/ui/quote/QuoteActivity.kt
@@ -1,12 +1,41 @@
 package com.pns.dailymotivationalquotes.ui.quote
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.pns.dailymotivationalquotes.R
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.pns.dailymotivationalquotes.databinding.ActivityQuoteBinding
+import com.pns.dailymotivationalquotes.utils.Resource
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class QuoteActivity : AppCompatActivity() {
+
+    private val viewModel: QuoteViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_quote)
+        val binding = ActivityQuoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val quoteAdapter = QuoteAdapter()
+
+        binding.apply {
+            recyclerView.apply {
+                adapter = quoteAdapter
+                layoutManager = LinearLayoutManager(this@QuoteActivity)
+            }
+            viewModel.quotes.observe(this@QuoteActivity) { result ->
+                quoteAdapter.submitList(result.data)
+
+                progressBar.isVisible = result is Resource.Loading && result.data.isNullOrEmpty()
+                textViewError.isVisible = result is Resource.Error && result.data.isNullOrEmpty()
+                textViewError.text = result.error?.localizedMessage
+            }
+        }
     }
 }
